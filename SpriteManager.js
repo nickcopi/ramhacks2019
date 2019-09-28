69cf720c78514959f995783f5b5e1e60bf009b43
nick <nick@localhost.localdomain>
2019-09-28 12:23:02 -0400

    Split up some classes into different files.

---
+++ SpriteManager.js
@@ -0,0 +1,6 @@
+class SpriteManager{
+	constructor(){
+
+	}
+
+}
--- game.js
+++ game.js
@@ -94,55 +94,6 @@ class House{
 	}
 }
 
-class Player{
-	constructor(x,y,width,height){
-		this.x = x;
-		this.y = y;
-		this.speed = 5;
-		this.width = width;
-		this.height = height;
-		this.direction = Directions.DOWN;
-		//this.img = spriteManager.player;
-	}
-	move(keys){
-		let found = false;
-		Keys.UP.forEach(k=>{
-			if(keys[k] && !found){
-				found = true;
-				this.y -= this.speed;
-				this.direction = Directions.UP;
-			}
-		});
-		found = false;
-		Keys.DOWN.forEach(k=>{
-			if(keys[k] && !found){
-				found = true;
-				this.y += this.speed;
-				this.direction = Directions.DOWN;
-			}
-		});
-		found = false;
-		Keys.LEFT.forEach(k=>{
-			if(keys[k] && !found){
-				found = true;
-				this.x -= this.speed;
-				this.direction = Directions.LEFT;
-			}
-		});
-		found = false;
-		Keys.RIGHT.forEach(k=>{
-			if(keys[k] && !found){
-				found = true;
-				this.x += this.speed;
-				this.direction = Directions.RIGHT;
-			}
-		});
-
-	}
-
-
-}
-
 
 
 
