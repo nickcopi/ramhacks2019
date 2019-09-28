c454a072ad19d214a9492b14b9fc6254ab80f854
root <root@realestategame.us-central1-a.c.ramhacks.internal>
2019-09-28 22:48:18 +0000

    flesh out SpriteManager somewhat

--- SpriteManager.js
+++ SpriteManager.js
@@ -1,6 +1,22 @@
 class SpriteManager{
-	constructor(){
+	
+	constructor() {
+		this.createSprite('road', 'https://google.com')
+		this.createSprite('house', 'https://google.com')
+	}
 
+	createSprite(name, src) {
+		this[name] = new Image()
+		this[name].src = src
+	}
+
+	house() {
+		return this['house']
+	}
+
+	road() {
+		return this['road']
 	}
 
 }
+
